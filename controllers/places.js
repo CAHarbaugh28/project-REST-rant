60cedf958991c36d043b38c14a191263b1641f58
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-06-13 20:00:26 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/', (req,res) => {
-    let places = [{
-        name: 'Jeffersons',
-        city: 'Lawrence',
-        state: 'KS',
-        cuisines: 'American',
-        pic: '/images/jeffersons.jpeg'
-      }, {
-        name: 'Hawaiian Bros',
-        city: 'Lawrence',
-        state: 'KS',
-        cuisines: 'Hawaiian',
-        pic: '/images/hawaiian-bros.jpeg'
-      }]
-      
+router.get('/', (req,res) => {   
     res.render('places/index', { places })
 })
 
@@ -23,8 +10,19 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Jeffersons',
+    city: 'Lawrence',
+    state: 'KS',
+    cuisines: 'American',
+    pic: '/images/jeffersons.jpeg'
+  }, {
+    name: 'Hawaiian Bros',
+    city: 'Lawrence',
+    state: 'KS',
+    cuisines: 'Hawaiian',
+    pic: '/images/hawaiian-bros.jpeg'
+  }]
