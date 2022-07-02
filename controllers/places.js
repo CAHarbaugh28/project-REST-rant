045ebf491b55f6bfdb060db68f902c820383324d
Carter Harbaugh <carter.harbaugh@gmail.com>
2022-07-02 17:54:32 -0500

    able to rant or rave

--- controllers/places.js
+++ controllers/places.js
@@ -29,7 +29,9 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -37,6 +39,32 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  req.body.rant = req.body.rant ? true : false
+  res.send('GET /places/:id/comment stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
